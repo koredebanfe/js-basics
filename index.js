d8c6c8b40c50594aa85eb49c688c88a4601969d6
koredebanfe <oluwakoredebanfe@gmail.com>
2023-02-12 14:50:15 -0800

    setting remote repo

---
+++ index.js
@@ -0,0 +1,16 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(2, 3));
+
+
+let firstName = "Oluwkorede";
+let lastName =  "Banfe";
+let stuternTrack = "Frontend";
+let skills = "JavaScript, Html, CSS";
+let favColour = "Yellow";
+let independence = 1960;
+
+console.log(`My name is ${firstName + lastName} and am a stutern student 
+skilled in  ${skills} with ${favColour} and from Nigeria which got her independence in ${independence}`);
---
+++ readme.md
@@ -0,0 +1,13 @@
+1. Node
+
+2. it adds interactions to web pages, build games 
+
+3. 
+
+4. console.log
+
+5. frontend, backend or fullstack
+
+6. statiscally typed programming language variables has a prticular data type attached to it e. Java. while variables in dynamically typed programming language can take any particular data type e.g javaScript
+
+7. null means nothing while undefined means no value yet.
